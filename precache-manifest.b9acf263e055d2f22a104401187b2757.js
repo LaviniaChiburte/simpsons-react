18a4a2d1f4a4d7d8d81d818d923080971c10a5b6
Lavinia Chiburte <lavinia.chiburte@gmail.com>
2019-09-24 08:20:09 +0300

    Updates

---
+++ precache-manifest.b9acf263e055d2f22a104401187b2757.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae38cd1bfb435c78b9234627863cf198",
+    "url": "/simpsons-react/index.html"
+  },
+  {
+    "revision": "429404ab828bfe0aeb3b",
+    "url": "/simpsons-react/static/css/main.de3b2276.chunk.css"
+  },
+  {
+    "revision": "00fb0dc5aca0bacfb4a9",
+    "url": "/simpsons-react/static/js/2.169b26ee.chunk.js"
+  },
+  {
+    "revision": "429404ab828bfe0aeb3b",
+    "url": "/simpsons-react/static/js/main.9e0fb53b.chunk.js"
+  },
+  {
+    "revision": "efdef716d46bf2e671b8",
+    "url": "/simpsons-react/static/js/runtime~main.9fcee108.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/simpsons-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
